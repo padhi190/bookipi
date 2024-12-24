e9a169bb797af4fc4df3d785930f36da88ae96e7
Pitra Pamungkas <pitra.adhi@gmail.com>
2024-12-24 14:03:57 +0700

    solution

--- src/components/AutocorrectTextarea.js
+++ src/components/AutocorrectTextarea.js
@@ -1,11 +1,40 @@
 import React from 'react';
 
 class AutocorrectTextarea extends React.Component {
+  state = {
+    text: ''
+  };
+
+  handleWordCorrection = (text) => {
+    const words = text.trim().split(' ');
+    const lastWord = words[words.length - 1].toLowerCase();
+    const correctedWord = this.props.corrections[lastWord];
+
+    if (!correctedWord) return text;
+
+    words[words.length - 1] = correctedWord;
+    return words.join(' ') + ' ';
+  };
+
+  handleChange = ({ target: { value } }) => {
+    const newText = value.endsWith(' ') 
+      ? this.handleWordCorrection(value)
+      : value;
+
+    this.setState({ text: newText });
+  };
 
   render() {
     return (
       <div className="text-center">
-        <textarea data-testid="textarea" rows={10} cols={80} className="card" />
+        <textarea 
+          data-testid="textarea"
+          rows={10}
+          cols={80}
+          className="card"
+          value={this.state.text}
+          onChange={this.handleChange}
+        />
       </div>
     );
   }
